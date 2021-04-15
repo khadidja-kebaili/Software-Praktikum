de3fe31412a85cecb4d8ec7e56af15fdeb5378d6
EsraCopuro <ec019@hdm-stuttgart.de>
2021-04-16 01:14:16 +0200

    Veränderungen in App.js, index.js, Matchmaker.js -> Habe eine Navigation auf der linken Seite erstellt.

--- Software-Praktikum/Frontend/lernapp/src/App.js
+++ Software-Praktikum/Frontend/lernapp/src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { makeStyles}  from "@material-ui/core/styles";
+import {BrowserRouter as Router,
+  Switch, Route, Link } from "react-router-dom";
+import 
+{Drawer, List, ListItem, 
+  ListItemIcon, ListItemText,
+  Container, Typography} 
+  from '@material-ui/core';
+import PersonIcon from '@material-ui/icons/Person';
+import ChatIcon from '@material-ui/icons/Chat';
 
-function App() {
+const matchmakerStyle = makeStyles ((theme) => ({
+  drawerPaper:{width:'inherit'},
+  link: {textDecoration: 'none', 
+  // Durch theme.palette.text.primary konnte der Link wieder ohne Unterstrich->blau angezeigt werden
+  color: theme.palette.text.primary }
+}))
+
+function App(){
+  const classes = matchmakerStyle();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div style={{display: 'flex'}}>
+        <Drawer
+        style={{width:'240px'}}
+        varian="persistent"
+        anchor="left"
+        open={true}
+        classes={{paper: classes.drawerPaper}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        {/* Hier wird jetzt ein Link erstellt, damit auf dem Router der Pfad steht */}
+          <List>
+          <Link to="/" className={classes.link} >
+            <ListItem button>
+              <ListItemIcon>
+                <PersonIcon/>
+              </ListItemIcon>
+              <ListItemText primary={"Profil"}/>
+            </ListItem>       
+          </Link>
+          <Link to="/chats" className={classes.link} >
+            <ListItem button>
+              <ListItemIcon>
+                <ChatIcon/>
+              </ListItemIcon>
+              <ListItemText primary={"Chats"}/>
+            </ListItem>       
+          </Link>
+          </List>
+          </Drawer>
+          <Switch>
+            <Route exact path="/">
+            <Container>
+              <Typography variant="h3" gutterBottum>
+                Profil
+              </Typography>
+            </Container>
+            </Route>
+            <Route exact path="/chats">
+              Chats
+            </Route>
+          </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
