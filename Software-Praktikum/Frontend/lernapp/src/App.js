9e02f7b2a6318f57df72e8e3324789f2bccb070f
EsraCopuro <ec019@hdm-stuttgart.de>
2021-04-16 23:25:05 +0200

    Neue Files hinzugefügt, damit es übersichtlicher wird. Zu den Files gehören: Navigation, Chats, Profil, Matchmaker.

--- Software-Praktikum/Frontend/lernapp/src/App.js
+++ Software-Praktikum/Frontend/lernapp/src/App.js
@@ -1,73 +1,118 @@
-import React from 'react';
-import './App.css';
-import { makeStyles}  from "@material-ui/core/styles";
-import {BrowserRouter as Router,
-  Switch, Route, Link } from "react-router-dom";
-import 
-{Drawer, List, ListItem, 
-  ListItemIcon, ListItemText,
-  Container, Typography} 
-  from '@material-ui/core';
-import PersonIcon from '@material-ui/icons/Person';
-import ChatIcon from '@material-ui/icons/Chat';
+// import React from 'react';
+// import './App.css';
+// import { makeStyles}  from "@material-ui/core/styles";
+// import {BrowserRouter as Router,
+//   Switch, Route, Link } from "react-router-dom";
+// import 
+// {Drawer, List, ListItem, 
+//   ListItemIcon, ListItemText,
+//   Container, Typography} 
+//   from '@material-ui/core';
+// import PersonIcon from '@material-ui/icons/Person';
+// import ChatIcon from '@material-ui/icons/Chat';
+// import GroupWorkIcon from '@material-ui/icons/GroupWork';
 
-const matchmakerStyle = makeStyles ((theme) => ({
-  drawerPaper:{width:'inherit'},
-  link: {textDecoration: 'none', 
-  // Durch theme.palette.text.primary konnte der Link wieder ohne Unterstrich->blau angezeigt werden
-  color: theme.palette.text.primary }
-}))
+
+// const matchmakerStyle = makeStyles ((theme) => ({
+//   drawerPaper:{width:'inherit'},
+//   link: {textDecoration: 'none', 
+//   // Durch theme.palette.text.primary konnte der Link wieder ohne Unterstrich->blau angezeigt werden
+//   color: theme.palette.text.primary }
+// }))
+
+// function App(){
+//   const classes = matchmakerStyle();
+//   return (
+//     <div>
+//       <h1>
+//         Hello
+//       </h1> 
+//       <Router>
+//       <div style={{display: 'flex'}}>
+//         <Drawer
+//         style={{width:'240px'}}
+//         varian="persistent"
+//         anchor="left"
+//         open={true}
+//         classes={{paper: classes.drawerPaper}}
+//         >
+//         {/* Hier wird jetzt ein Link erstellt, damit auf dem Router der Pfad steht */}
+//           <List>
+//           <Link to="/profil" className={classes.link} >
+//             <ListItem button>
+//               <ListItemIcon>
+//                 <PersonIcon/>
+//               </ListItemIcon>
+//               <ListItemText primary={"Profil"}/>
+//             </ListItem>       
+//           </Link>
+//           <Link to="/chats" className={classes.link} >
+//             <ListItem button>
+//               <ListItemIcon>
+//                 <ChatIcon/>
+//               </ListItemIcon>
+//               <ListItemText primary={"Chats"}/>
+//             </ListItem>       
+//           </Link>
+//           <Link to="/matchmaker" className={classes.link} >
+//             <ListItem button>
+//               <ListItemIcon>
+//                 <GroupWorkIcon/>
+//               </ListItemIcon>
+//               <ListItemText primary={"Matchmaker"}/>
+//             </ListItem>       
+//           </Link>
+//           </List>
+//           </Drawer>
+//           <Switch>
+//             <Route exact path="/profil">
+//             <Container>
+//               <Typography variant="h3" gutterBottum>
+//                 Profil
+//               </Typography>
+//             </Container>
+//             </Route>
+//             <Route exact path="/chats">
+//               Chats
+//             </Route>
+//             <Route exact path="/matchmaker">
+//               Matchmaker
+//             </Route>
+//           </Switch>
+//       </div>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import "./App.css";
+import Matchmaker from "./Matchmaker";
+import Profil from "./Profil";
+import Chats from "./Chats";
+import Navigation from './Navigation';
+import {BrowserRouter as Router,
+    Switch, Route, Link } from "react-router-dom";
+import {makeStyles} from "@material-ui/styles";
+
+const useStyle = makeStyles ({});
 
 function App(){
-  const classes = matchmakerStyle();
+  const classes = useStyle();
   return (
-    <Router>
-      <div style={{display: 'flex'}}>
-        <Drawer
-        style={{width:'240px'}}
-        varian="persistent"
-        anchor="left"
-        open={true}
-        classes={{paper: classes.drawerPaper}}
-        >
-        {/* Hier wird jetzt ein Link erstellt, damit auf dem Router der Pfad steht */}
-          <List>
-          <Link to="/" className={classes.link} >
-            <ListItem button>
-              <ListItemIcon>
-                <PersonIcon/>
-              </ListItemIcon>
-              <ListItemText primary={"Profil"}/>
-            </ListItem>       
-          </Link>
-          <Link to="/chats" className={classes.link} >
-            <ListItem button>
-              <ListItemIcon>
-                <ChatIcon/>
-              </ListItemIcon>
-              <ListItemText primary={"Chats"}/>
-            </ListItem>       
-          </Link>
-          </List>
-          </Drawer>
-          <Switch>
-            <Route exact path="/">
-            <Container>
-              <Typography variant="h3" gutterBottum>
-                Profil
-              </Typography>
-            </Container>
-            </Route>
-            <Route exact path="/chats">
-              Chats
-            </Route>
-          </Switch>
-      </div>
-    </Router>
-    
+    <div className={classes.container}>
+      <Navigation/>
+      <Router>
+      <Switch>
+        <Route exact from="/" render={props => <Matchmaker{...props}/>}/>
+        <Route exact path="/profil" render={props => <Profil{...props}/>}/>
+        <Route exact path="/chats" render={props => <Chats{...props}/>}/>
+      </Switch>
+      </Router>
+    </div>
   );
 }
 
 export default App;
-
-
---
+++ Software-Praktikum/Frontend/lernapp/src/Navigation.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import {withRouter} from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Navigation = props => {
+  const {history}=props;
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClick = (newPage) => {
+    history.pushState(newPage);
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      
+      <AppBar position="static">
+        <Toolbar>
+         
+          <Typography variant="h6" className={classes.title}>
+            Photos
+          </Typography>
+         
+            <div>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+                onClick={handleMenu}
+                
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+              >
+                <MenuItem onClick={() => handleMenuClick('./Matchmaker')}>Matchmaker</MenuItem>
+                <MenuItem onClick={() => handleMenuClick('./Profil')}>Profil</MenuItem>
+                <MenuItem onClick={() => handleMenuClick('./Chats')}>Chats</MenuItem>
+              </Menu>
+            </div>
+       
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+export default Navigation;
