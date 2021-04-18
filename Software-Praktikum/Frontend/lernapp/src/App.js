2cc7e280ba1dae4702ead64b6b831196c0c1c9ed
Mihriban Dogan <md121@hdm-stuttgart.de>
2021-04-18 10:41:59 +0200

    Hinzufügen von Eventhandler

--- Software-Praktikum/Frontend/lernapp/src/App.js
+++ Software-Praktikum/Frontend/lernapp/src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import UserProfil from './Components/Dialog/Userprofil_form.js';
+import UserProfilprobe from './Components/Dialog/probe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProfil></UserProfil>
+    // <UserProfilprobe></UserProfilprobe>
   );
 }
 
--- Software-Praktikum/Frontend/lernapp/src/Components/Dialog/Userprofil_form.js
+++ Software-Praktikum/Frontend/lernapp/src/Components/Dialog/Userprofil_form.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
-import Container from '@material-ui/core/Container';
-import "./style.userprofile.css"
+import "./Userprofil_form.css"
 import Button from '@material-ui/core/Button';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import Select from '@material-ui/core/Select';
+import { FormControl } from '@material-ui/core';
 
 class UserProfil extends Component {
   constructor(props){
-  super(props)
+  super(props);
   this.state = { 
     name: "",
     vorname:"",
@@ -32,90 +30,109 @@ class UserProfil extends Component {
     lernfrequenz:"",
     vorkentnisse:"",
     berufserfahrung:""
-   }
+   };
+   this.handleChange = this.handleChange.bind(this);
   }
-  handleChange = () =>{
-    
-  }
+  handleChange = (e) =>{
+    this.setState({ [e.target.name] : e.target.value });}
+
+ submitHandler= (e) =>{
+   e.preventDefault()
+   console.log(this.state)
+  //  fetch("http://127.0.0.1:5000/hello", {
+  //   method: 'POST',
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     "name": this.state.name
+  //   })
+  // })
+ }
+
   render() { 
     return ( 
       <div>
-      <Container className="Überschrift">
+      <div className="Überschrift">
         <h1>Erstelle dein Profil und finde deine Lerngruppe!</h1>
-      </Container>
-          <form>
-            <div className="PersProfil"> 
-            <h2> Persönliches Profil</h2>
-              <div className="Name"><TextField id="Name" label="Name" variant="outlined" value={this.state.name} onChange={this.handleChange}/></div>
-              <div className="Vorname"><TextField id="Vorname" label="Vorname" variant="outlined"/> </div>
-              <div className="Alter"> <TextField id="Alter" label="Alter" variant="outlined"/> </div>
-              <div className="Semester"><TextField id="Semester" label="Semester" variant="outlined" /></div>
-              <div className="Studiengang"><TextField fullwidth id="Studiengang" label="Studiengang" variant="outlined" /></div>
-              <div className="Hobbies"><TextField id="Hobbies" label="Hobbies" variant="outlined"/></div>
-              <div className="Interessen"><TextField id="Interessen" label="Interessen" variant="outlined"/></div>
-              <div className="Vorlieben"><TextField id="Vorlieben" label="Vorlieben" variant="outlined"/></div>
-               <div className="extro">
-                  <FormControl component="fieldset">
-                  <FormLabel component="legend">Persönlichkeit (introvertiert 1 - extrovertiert 5)</FormLabel>
-                  <RadioGroup aria-label="persönlichkeit" name="persönlichkeit" row>
+      </div>
+          <form onSubmit={this.submitHandler}>
+          <div className="PersProfil"> 
+              <div className="Name"><TextField name="name" label="Name" variant="outlined" value ={this.state.name} onChange={this.handleChange}/></div>
+              <div className="Vorname"><TextField name="vorname" label="Vorname" variant="outlined" value ={this.state.vorname} onChange={this.handleChange}/> </div>
+              <div className="Alter"> <TextField name="alter" label="Alter" variant="outlined" value ={this.state.alter} onChange={this.handleChange}/> </div>
+              <FormControl className="Semester">
+              <InputLabel id="label"> &nbsp; Semester</InputLabel>
+              <Select
+                    labelId="label" id="select"
+                    name="semester" variant="outlined" value={this.state.semester} 
+                    onChange={this.handleChange}>
+                    <MenuItem value={1}>1.</MenuItem> 
+                    <MenuItem value={2}>2.</MenuItem>
+                    <MenuItem value={3}>3.</MenuItem>
+                    <MenuItem value={4}>4.</MenuItem>
+                    <MenuItem value={5}>5.</MenuItem>
+                    <MenuItem value={6}>6.</MenuItem>
+                    <MenuItem value={7}>7.</MenuItem>
+              </Select>
+              </FormControl>
+              <div className="Studiengang"><TextField fullwidth="true" name="studiengang" label="Studiengang" variant="outlined" value ={this.state.studiengang} onChange={this.handleChange} /></div>
+              <div className="Hobbies"><TextField name="hobbies" label="Hobbies" variant="outlined"value ={this.state.hobbies} onChange={this.handleChange}/></div>
+              <div className="Interessen"><TextField name="interessen" label="Interessen" variant="outlined" value ={this.state.interessen} onChange={this.handleChange}/></div>
+              <div className="Vorlieben"><TextField name="vorlieben" label="Vorlieben" variant="outlined" value ={this.state.vorlieben} onChange={this.handleChange}/></div>
+              <div className="radio">
+              <FormLabel component="legend">Persönlichkeit (introvertiert 1 - extrovertiert 5)</FormLabel>
+                    <RadioGroup aria-label="persönlichkeit" name="persönlichkeit"  row value={this.state.persönlichkeit} onChange={this.handleChange} >
                     <FormControlLabel value="1" control={<Radio />} label="1" />
                     <FormControlLabel value="2" control={<Radio />} label="2" />
                     <FormControlLabel value="3" control={<Radio />} label="3" />
                     <FormControlLabel value="4" control={<Radio />} label="4"/>
                     <FormControlLabel value="5" control={<Radio />} label="5" />
-                  </RadioGroup>
+                    </RadioGroup>
+              </div>
+              </div>
+              <div className="Lernprofil">
+              <h2>Lernprofil</h2>
+              <FormControl className="Lerntyp">
+              <InputLabel> &nbsp; Lerntyp</InputLabel>
+              <Select
+                    name="lerntyp" variant="outlined" value={this.state.lerntyp} 
+                    onChange={this.handleChange}>
+                    <MenuItem value={"auditiv"}>auditiv</MenuItem> 
+                    <MenuItem value={"kommunikativ"}>kommunikativ</MenuItem>
+                    <MenuItem value={"motorisch"}>motorisch</MenuItem>
+                    <MenuItem value={"visuell"}>visuell</MenuItem>
+              </Select>
+              </FormControl>
+              <FormControl className="Lernzeitraum">
+              <InputLabel> &nbsp; Bevorzugter Lernzeitraum</InputLabel>
+              <Select
+                    name="lernzeitraum" variant="outlined"
+                    value={this.state.lernzeitraum} 
+                    onChange={this.handleChange}>
+                    <MenuItem value={"Morgens"}>Morgens</MenuItem> 
+                    <MenuItem value={"Mittags"}>Mittags</MenuItem>
+                    <MenuItem value={"Abends"}>Abends</MenuItem>
+                    <MenuItem value={"Flexibel"}>Flexibel</MenuItem>
+              </Select>
+              </FormControl>
+              <FormControl className="Lernort">
+              <InputLabel> &nbsp; Bevorzugter Lernort</InputLabel>
+                <Select
+                    name="lernort" variant="outlined"
+                    value={this.state.lernort} 
+                    onChange={this.handleChange}>
+                    <MenuItem value={"Universität"}>Universität</MenuItem> 
+                    <MenuItem value={"Online Meetings"}>Online Meetings</MenuItem>
+                </Select>
                 </FormControl>
-        </div>
-           </div>
-           <div className="Lernprofil">
-            <h2>Lernprofil</h2>
-            <div className="Lerntyp">
-                <FormControl className="probe">
-                  <InputLabel id="demo-controlled-open-select-label"> &nbsp; Lerntyp</InputLabel>
-                  <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select" variant="outlined">
-                      {/* VALUE MUSS NOCH ANGEPASST WERDEN!!!! */}
-                    <MenuItem value={1}>auditiv</MenuItem> 
-                    <MenuItem value={2}>kommunikativ</MenuItem>
-                    <MenuItem value={3}>motorisch</MenuItem>
-                    <MenuItem value={4}>visuell</MenuItem>
-                  </Select>
-                </FormControl>
-                </div>
-            <div className="Lernzeitraum">
-                <FormControl className="probe">
-                  <InputLabel id="demo-controlled-open-select-label"> &nbsp; Bevorzugter Lernzeitpunkt</InputLabel>
-                  <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select" variant="outlined">
-                      {/* VALUE MUSS NOCH ANGEPASST WERDEN!!!! */}
-                    <MenuItem value={1}>Morgens</MenuItem> 
-                    <MenuItem value={2}>Mittags</MenuItem>
-                    <MenuItem value={3}>Abends</MenuItem>
-                    <MenuItem value={4}>Flexibel</MenuItem>
-                  </Select>
-                </FormControl>
-                </div>
-            <div className="Lernort">
-                <FormControl className="probe">
-                  <InputLabel id="demo-controlled-open-select-label"> &nbsp; Bevorzugter Lernort</InputLabel>
-                  <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select" variant="outlined">
-                      {/* VALUE MUSS NOCH ANGEPASST WERDEN!!!! */}
-                    <MenuItem value={1}>Universität</MenuItem> 
-                    <MenuItem value={2}>Online Meetings</MenuItem>
-                  </Select>
-                </FormControl>
-                </div>
-            <div className="Lernfrequenz">
-                <FormControl className="probe">
-                  <InputLabel id="demo-controlled-open-select-label"> &nbsp; Bevorzugte Lernfrequenz</InputLabel>
-                  <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select" variant="outlined">
-                      {/* VALUE MUSS NOCH ANGEPASST WERDEN!!!! */}
+                <FormControl className="Lernfrequenz">
+                <InputLabel> &nbsp; Bevorzugte Lernfrequenz</InputLabel>
+                <Select
+                    name="lernfrequenz" variant="outlined"
+                    value={this.state.lernfrequenz} 
+                    onChange={this.handleChange}>
                     <MenuItem value={1}>1x pro Woche</MenuItem> 
                     <MenuItem value={2}>2x pro Woche</MenuItem>
                     <MenuItem value={3}>3x pro Woche</MenuItem>
@@ -123,14 +140,13 @@ class UserProfil extends Component {
                     <MenuItem value={5}>5x pro Woche</MenuItem>
                     <MenuItem value={6}>6x pro Woche</MenuItem>
                     <MenuItem value={7}>7x pro Woche</MenuItem>
-                  </Select>
+                </Select>
                 </FormControl>
-                </div>
-            <div className="Vorkenntnisse"><TextField id="standard-basic" label="Vorkenntnisse" variant="outlined"/></div>
-            <div className="Berufserfahrung"><TextField id="standard-basic" label="Berufserfahrung" variant="outlined" /></div>
-          </div>
-          </form>
-         <div className="Anmelden"><Button variant="contained" color="primary" size="large"> Anmelden</Button></div>
+                <div className="Vorkenntnisse"><TextField name="vorkenntnisse" label="Vorkenntnisse" variant="outlined" value ={this.state.vorkenntnisse} onChange={this.handleChange}/></div>
+                <div className="Berufserfahrung"><TextField name="berufserfahrung" label="Berufserfahrung" variant="outlined"value ={this.state.berufserfahrung} onChange={this.handleChange} /></div>
+               </div>
+            <div className="Anmelden"><Button type="submit" variant="contained" color="primary" size="large"> Anmelden</Button></div>
+           </form>
        </div>
    
       );
@@ -138,3 +154,4 @@ class UserProfil extends Component {
 }
 
 export default UserProfil;
+
