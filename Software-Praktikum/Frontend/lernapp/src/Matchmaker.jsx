0eccf135758d52a2b617a0ffee00cdd902b575cc
EsraCopuro <ec019@hdm-stuttgart.de>
2021-04-22 22:48:38 +0200

    Liste wurde in Matchmaker.jsx erstellt, doch die Funktionen fehlen.

--- Software-Praktikum/Frontend/lernapp/src/Matchmaker.jsx
+++ Software-Praktikum/Frontend/lernapp/src/Matchmaker.jsx
@@ -47,12 +47,6 @@
 // import FavoriteIcon from '@material-ui/icons/Favorite';
 // import { List } from '@material-ui/core';
 
-// // const Matchmaker = props => {
-// //     const listMatches = props;
-// //     const showNextMatch = (newMatch) => {
-// //        listMatches.pushState(newMatch)
-    
-//     // };
 
 // function Matchmaker(){
 //     return (
@@ -65,51 +59,82 @@
 
 // export default Matchmaker; 
 
-import { StylesContext } from '@material-ui/styles';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Swiper from 'react-native-deck-swiper';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
-const Picture = ({picture}) => (
-    <View style={style.card}>
-        <Image source={{uri:picture.image}} style={styles.pictureProfil}/>
-    </View>
-)
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
 
-
-export default function Matchmaker(){
-    return(
-        <View style={styles.container}>
-            <Swiper
-                cards={}
-                cardIndex={}
-                renderCard={(picture) => <Picture picture={picture}/>}
-            />
-        </View>
-    )
+function generate(element) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
 }
 
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor:'#fff',
-        alignItems:'center',
-        justifyContent:'center'   
-    },
-    picture:{
-        flex: 0.45,
-        borderRadius: 8,
-        shadowRadius: 25,
-        shadowColor: '#000',
-        shadowOffset: {width:0, height:0},
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#fff'
-    },
-    pictureProfil:{
-        width: 100,
-        flex: 1,
-        resizeMode: 'contain'
-    }
-});
+export default function InteractiveList() {
+  const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
+  return (
+    <div className={classes.root}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h6" className={classes.title}>
+            Avatar with text and icon
+          </Typography>
+          <div className={classes.demo}>
+            <List dense={dense}>
+              {generate(
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <AccountCircleIcon/>
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Single-line item"
+                    secondary={secondary ? 'Secondary text' : null}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <AddIcon/>
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>,
+              )}
+            </List>
+          </div>
+        </Grid>
     
+    </div>
+  );
+}
