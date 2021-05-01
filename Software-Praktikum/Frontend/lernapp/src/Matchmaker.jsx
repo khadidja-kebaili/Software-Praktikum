4a19521627cf5e76c7ad51eb00dcabb9e32ac36c
EsraCopuro <ec019@hdm-stuttgart.de>
2021-05-01 02:54:34 +0200

    Neue Files wurden hinzugefügt.

--- Software-Praktikum/Frontend/lernapp/src/Matchmaker.jsx
+++ Software-Praktikum/Frontend/lernapp/src/Matchmaker.jsx
@@ -1,63 +1,3 @@
-// import React, {Component} from "react";
-// import {Link, Switch, Route} from "react-router-dom";
-// import "./App.css"
-
-// const Matchmaking = () => <div>Du bist auf Matchmaking!</div> 
-// const Chat = () => <div>Du bist auf Chat!</div>
-// const Profil = () => <div>Du bist auf Profil!</div>
-
-// class Matchmaker extends Component {
-//   render(){
-//     return(
-//       <div>
-//         <h1>Hey du bist auf der Matchmaking-Seite!</h1>
-        
-//         <div className="tabs">
-//           <br/>
-//           <hr/>
-//           <Switch>
-//             <Route path="/matchmaking" exact component ={Matchmaking} />
-//             <Route path="/matchmaking/chat" exact component ={Chat} />
-//             <Route path="/matchmaking/profil" exact component ={Profil} />
-
-//           </Switch>
-//         </div>
-//         <div className="herz"> </div>
-//         <button></button>
-//         <div className="kreuz">
-          
-//         </div>
-        
-        
-//         <div className="links">
-//           <Link to = "/matchmaking" className="link"> Matchmaking </Link>
-//           <Link to = "/matchmaking/chat" className="link"> Chat </Link>
-//           <Link to = "/matchmaking/profil" className="link"> Profil </Link>
-//         </div>
-        
-//       </div>
-//     )
-//   }
-// }
-
-// export default Matchmaker;
-
-// import React from 'react';
-// import './Matchmaker.css';
-// import FavoriteIcon from '@material-ui/icons/Favorite';
-// import { List } from '@material-ui/core';
-
-
-// function Matchmaker(){
-//     return (
-//         <div className="Matches">
-//             {/* <FavoriteIcon onClick={() => showNextMatch()}/> */}
-//             <FavoriteIcon/>
-//         </div>
-//     )
-// };
-
-// export default Matchmaker; 
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
@@ -140,3 +80,4 @@ export default function InteractiveList() {
     
   );
 }
+
---
+++ Software-Praktikum/src/server/bo/ProfilBO.py
@@ -0,0 +1,125 @@
+from .Businessobject import BusinessObject
+
+
+class Studentprofil (BusinessObject):
+    def __init__(self):
+        super().__init__()
+        self.name = "",
+        self.vorname = "",
+        self.alter = "",
+        self.semester = "",
+        self.studiengang = "",
+        self.hobbies = "",
+        self.interessen = "",
+        self.persönlichkeit = "",
+        self.lerntyp = "",
+        self.lernzeitraum = "",
+        self.lernort = "",
+        self.lernfrequenz = "",
+        self.berufserfahrung = ""
+
+    def set_name(self, value):
+        self.name = value
+
+    def get_name(self):
+        return self.name
+
+    def set_vorname(self, value):
+        self.vorname = value
+
+    def get_vorname(self):
+        return self.vorname
+
+    def set_alter(self, value):
+        self.alter = value
+
+    def get_alter(self):
+        return self.alter
+
+    def get_semester(self):
+        """Auslesen des Benutzernamens."""
+        return self.semester
+
+    def set_semester(self, value):
+        """Setzen des Benutzernamens."""
+        self.semester = value
+
+    def get_studiengang(self):
+        """Auslesen des Benutzernamens."""
+        return self.studiengang
+
+    def set_studiengang(self, value):
+        """Setzen des Benutzernamens."""
+        self.studiengang = value
+
+    def set_hobbies(self, value):
+        self.hobbies = value
+
+    def get_hobbies(self):
+        return self.hobbies
+
+    def set_interessen(self, value):
+        self.interessen = value
+
+    def get_interessen(self):
+        return self.interessen
+
+    def set_persönlichkeit(self, value):
+        self.persönlichkeit = value
+
+    def get_persönlichkeit(self):
+        return self.persönlichkeit
+
+    def set_lerntyp(self, value):
+        self.lerntyp = value
+
+    def get_lerntyp(self):
+        return self.lerntyp
+
+    def set_lernzeitraum(self, value):
+        self.lernzeitraum = value
+
+    def get_lernzeitraum(self):
+        return self.lernzeitraum
+
+    def set_lernort(self, value):
+        self.lernort = value
+
+    def get_lernort(self):
+        return self.lernort
+
+    def set_lernfrequenz(self, value):
+        self.lernfrequenz = value
+
+    def get_lernfrequenz(self):
+        return self.lernfrequenz
+
+    def set_berufserfahrung(self, value):
+        self.berufserfahrung = value
+
+    def get_berufserfahrung(self):
+        return self.berufserfahrung
+
+    def __str__(self):
+        """Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz."""
+        return "User: {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(self.get_name(), self.get_vorname(), self.get_alter(), self.get_semester(), self.get_studiengang(), self.get_hobbies(), self.get_interessen(), self.get_persönlichkeit(), self.get_lernort(), self.get_lernzeitraum(), self.get_lernfrequenz(), self.get_lerntyp(), self.get_berufserfahrung())
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        """Umwandeln eines Python dict() in einen User()."""
+        obj = Studentprofil()
+        obj.set_id(dictionary["id"])
+        obj.set_name(dictionary["name"])
+        obj.set_vorname(dictionary["vorname"])
+        obj.set_alter(dictionary["alter"])
+        obj.set_semester(dictionary["semester"])
+        obj.set_studiengang(dictionary["studiengang"])
+        obj.set_hobbies(dictionary["hobbies"])
+        obj.set_interessen(dictionary["interessen"])
+        obj.set_persönlichkeit(dictionary["persönlichkeit"])
+        obj.set_lerntyp(dictionary["lerntyp"])
+        obj.set_lernzeitraum(dictionary["lernzeitraum"])
+        obj.set_lernort(dictionary["lernort"])
+        obj.set_lernfrequenz(dictionary["lernfrequenz"])
+        obj.set_berufserfahrung(dictionary["berufserfahrung"])
+        return obj
