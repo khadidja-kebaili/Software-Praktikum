472b1f140226e1591d0baece7733aa42873f3dc5
Mihriban Dogan <md121@hdm-stuttgart.de>
2021-05-02 00:39:44 +0200

    Firebase Probe klappt

--- Software-Praktikum/Frontend/lernapp/src/App.test.js
+++ Software-Praktikum/Frontend/lernapp/src/App.test.js
@@ -1,8 +1,131 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { Container} from '@material-ui/core';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import SignIn from './Components/Pages/SignIn';
+import firebaseConfig from './firebaseConfig';
+import Profilanzeigen from './Components/Pages/Profil_anzeigen'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+class App extends React.Component {
+
+	/** Constructor of the app, which initializes firebase  */
+	constructor(props) {
+		super(props);
+
+		// Init an empty state
+		this.state = {
+			currentUser: null,
+			appError: null,
+			authError: null,
+			authLoading: false
+		};
+	}
+
+	/** 
+	 * Create an error boundary for this app and recieve all errors from below the component tree.
+	 * 
+	 * @See See Reacts [Error Boundaries](https://reactjs.org/docs/error-boundaries.html)
+ 	 */
+	static getDerivedStateFromError(error) {
+		// Update state so the next render will show the fallback UI.
+		return { appError: error };
+	}
+
+	/** Handles firebase users logged in state changes  */
+	handleAuthStateChange = user => {
+		if (user) {
+			this.setState({
+				authLoading: true
+			});
+			// The user is signed in
+			user.getIdToken().then(token => {
+				// Add the token to the browser's cookies. The server will then be
+				// able to verify the token against the API.
+				// SECURITY NOTE: As cookies can easily be modified, only put the
+				// token (which is verified server-side) in a cookie; do not add other
+				// user information.
+				document.cookie = `token=${token};path=/`;
+
+				// Set the user not before the token arrived 
+				this.setState({
+					currentUser: user,
+					authError: null,
+					authLoading: false
+				});
+			}).catch(e => {
+				this.setState({
+					authError: e,
+					authLoading: false
+				});
+			});
+		} else {
+			// User has logged out, so clear the id token
+			document.cookie = 'token=;path=/';
+
+			// Set the logged out user to null
+			this.setState({
+				currentUser: null,
+				authLoading: false
+			});
+		}
+	}
+
+  /** 
+   * Handles the sign in request of the SignIn component uses the firebase.auth() component to sign in.
+	 * @see See Google [firebase.auth()](https://firebase.google.com/docs/reference/js/firebase.auth.Auth)
+	 * @see See Google [firebase.auth().signInWithRedirect](https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signinwithredirect)
+	 */
+	handleSignIn = () => {
+		this.setState({
+			authLoading: true
+		});
+		const provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithRedirect(provider);
+	}
+
+	/**
+	 * Lifecycle method, which is called when the component gets inserted into the browsers DOM.
+	 * Initializes the firebase SDK.
+	 * 
+	 * @see See Googles [firebase init process](https://firebase.google.com/docs/web/setup)
+	 */
+	componentDidMount() {
+		firebase.initializeApp(firebaseConfig);
+		firebase.auth().languageCode = 'en';
+		firebase.auth().onAuthStateChanged(this.handleAuthStateChange);
+	}
+
+	/** Renders the whole app */
+	render() {
+		const { currentUser} = this.state;
+
+		return (
+				<Router >
+					<Container maxWidth='md'>
+						< Container user={currentUser} />
+						{
+							// Is a user signed in?
+							currentUser ?
+								<>
+									<Redirect from='/' to='profil' />
+									<Route exact path='/profil'>
+										<Profilanzeigen />
+									</Route>
+								</>
+								:
+								// else show the sign in page
+								<>
+									<Redirect to='/index.html' />
+									<SignIn onSignIn={this.handleSignIn} />
+								</>
+						}
+					</Container>
+				</Router>
+		);
+	}
+}
+
+export default App;
