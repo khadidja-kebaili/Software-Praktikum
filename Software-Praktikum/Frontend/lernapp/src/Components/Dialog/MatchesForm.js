3c2d5353316d2e019e1da08e1dfc4ec8a5c0cab8
EsraCopuro <ec019@hdm-stuttgart.de>
2021-05-01 02:29:00 +0200

    Versuch: Die Files miteinander verbinden.

--- Software-Praktikum/Frontend/lernapp/src/Components/Dialog/MatchesForm.js
+++ Software-Praktikum/Frontend/lernapp/src/Components/Dialog/MatchesForm.js
@@ -2,4 +2,4 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, Button, IconButton, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import { LernAppAPI, ProfilBO } from './';
+import { LernappAPI, ProfilBO } from '../../';
