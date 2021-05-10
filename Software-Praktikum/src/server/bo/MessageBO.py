b64edfcbaa5a086ef31b5c953aca861ad39abe8d
HamiDuong <hd028@hdm-stuttgart.de>
2021-05-10 15:28:52 +0200

    Nachrichten werden mit Zeitstempeln versehen

--- Software-Praktikum/src/server/bo/MessageBO.py
+++ Software-Praktikum/src/server/bo/MessageBO.py
@@ -1,36 +1,48 @@
 from .Businessobject import Businessobject;
 
-class Message (Businessobject):
+class MessageBO (Businessobject):
     #Instanziierung des Objekts der Klasse Message
-    def __init__(self, senderID, room, text, roomCounter):
+    def __init__(self):
         super().__init__()
-        self.senderID = senderID;
-        self.room = room;
-        self.text = text;
-        self.roomCounter = roomCounter;
+        self.profilID = None;
+        self.room = None;
+        self.text = None;
+        self.time = None;
 
-    def get_senderID(self):
-        return this.senderID;
+    def get_profilID(self):
+        return self.senderID;
+
+    def set_profilID(self, id):
+        self.profilID = id;
 
     def get_room(self):
-        return this.room;
+        return self.room;
+
+    def set_room(self, room):
+        self.room = room;
 
     def get_text(self):
-        return this.text;
+        return self.text;
+
+    def set_text(self, text):
+        self.text = text;
     
-    def get_roomCounter(self):
-        return this.roomCounter;
+    def get_time(self):
+        return self.time;
+
+#    def set_time(self):
+#        this.time = datetime.now(tz = None);
      
+    def __str__(self):
+        return "Id: {}, Message von Profil {} im Chat {}: {} (Zeit: {})".format(self.get_id(), self.get_profilID(), self.get_room(), self.get_text(), self.get_time());
+
 #Umwandlung eines Dictonary Eintrags in ein Message Objekt
 @staticmethod
 def from_dict(dictionary=dict()):
-
- alte Version
-    obj = Message(
-        dictionary["id"],
-        dictionary["senderID"],
-        dictionary["room"],
-        dictionary["text"],
-        dictionary["roomCounter"]
-    );
+    obj = MessageBO();
+    obj.set_id(dictionary["id"]);
+    obj.set_profilID(dictionary["profilID"]);
+    obj.set_room(dictionary["room"]);
+    obj.set_text(dictionary["text"]);
+    obj.set_time(dictionary["time"]);
     return obj;
