2fb0b1d7dfd452b6cc942deb67eb46e4bdafb780
Sarah Rowan <80517568+sr168@users.noreply.github.com>
2021-05-11 13:35:23 +0200

    createGroup

---
+++ Software-Praktikum/Frontend/lernapp/src/Components/Gruppe.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Gruppe extends Component {
+    state = {  }
+    render() { 
+        return (<h1>Hello</h1>);
+    }
+    
+}
+ 
+export default Gruppe;
