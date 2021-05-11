2cc1fc826b284d1c02ffa3cb4bf9743220d80b3b
Lena <lt035@hdm-stuttgart.de>
2021-05-11 13:46:19 +0200

    Member delete angepasst

--- Software-Praktikum/Frontend/lernapp/src/Components/Dialog/MemberDelete.js
+++ Software-Praktikum/Frontend/lernapp/src/Components/Dialog/MemberDelete.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
-import { ElectivAPI } from '../../api';
-import Snackbar from '@material-ui/core/Snackbar';
-import Alert from '@material-ui/lab/Alert';
+import Button from '@material-ui/core/Button';
+import LernappAPI from "../../API/LernappAPI";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
 class MemberDelete extends Component {
@@ -11,67 +13,43 @@ class MemberDelete extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+    };
+  }
 
+  deleteMember = () => {
+    let data = 6;
+    LernappAPI.getAPI().deleteMember(data).then(this.props.onClose(null))
+    };
 
-  //wird aufgerufen, wenn das Dialog geschlossen wird
   handleClose = () => {
+    // console.log(event);
     this.props.onClose(null);
   }
 
-  // API Anbindung um die Gruppe über das Backend aus der Datenbank zu löschen
-deleteMember = () => {
-  ElectivAPI.getAPI().deleteMember(this.state.M.id)
-    .then(() => {
-      this.props.getMember();
-      this.props.onClose(null);
-    }).catch(e => {
-      this.setState({
-        showSnackbar: true
-      })
-    })
-}
-  // Die Komponente wird gerendert 
-  render() 
-    const { show } = this.props;
-    const { Member } = this.state;
-    return (
-      <div>
-        <Dialog
-          open={show}
-          onClose={this.handleClose}
-          maxWidth='xs'
-        >
-          <DialogTitle>{"Sind Sie sich sicher?"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Wenn Sie dieses Mitglied <b>"{Member.name}"</b> löschen möchten, klicken Sie auf "JA".
-                  </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Abbrechen
-                  </Button>
-            <Button onClick={this.deleteMember} color="primary" autoFocus>
-              Ja
-                  </Button>
-          </DialogActions>
-        </Dialog>
-        <Snackbar open={showSnackbar} autoHideDuration={6000} onClose={this.closeSnackbar}>
-          <Alert onClose={this.closeSnackbar} severity="error">
-            Dieses Mitglied kann nicht gelöscht werden
-                </Alert>
-        </Snackbar>
-      </div>
+  /** Renders the component */
+  render() {
+
+        return (
+          <div>
+            <Dialog open={this.props.show}>
+              <DialogTitle id="alert-dialog-title">{"Willst du dieses Mitglied wirklich löschen?"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Wenn du auf Löschen klickst wird dieses Mitglied endgültig gelöscht. 
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button color="primary" onClick={this.props.onClose} >
+                 Abbrechen
+                </Button>
+                <Button color="primary" onClick={this.deleteMember} autoFocus>
+                  Löschen
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
     );
   }
-
 }
-// PropTypes 
-MemberDelete.propTypes = {
-  
-  classes: PropTypes.object.isRequired,
-  show: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
-}
-
-export default withStyles(styles)(MemberDelete);
+export default DeleteMember;
