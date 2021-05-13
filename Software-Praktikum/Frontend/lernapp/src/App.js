e6f9c4085b4b1b784b796bfd4342c10e466e4ebc
HamiDuong <hd028@hdm-stuttgart.de>
2021-05-13 18:07:16 +0200

    App.js soll Chatlist rendern zum test

--- Software-Praktikum/Frontend/lernapp/src/App.js
+++ Software-Praktikum/Frontend/lernapp/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
 import React from 'react';
-import Chatroom from './Components/Pages/Chatroom.js'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
+import Chatlist from './Components/ChatList';
+
+//hier wird current user gespeichert
 
 export default function App(){
     return(
         <div>
-            <Chatroom/>
+            <Chatlist/>
         </div>
     )
 }
