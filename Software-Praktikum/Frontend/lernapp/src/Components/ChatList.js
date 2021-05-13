a8f09204dbc7ef702965b61d486df4fadc802a83
HamiDuong <hd028@hdm-stuttgart.de>
2021-05-13 17:55:25 +0200

    Componente ChatList erstellt

--- Software-Praktikum/Frontend/lernapp/src/Components/ChatList.js
+++ Software-Praktikum/Frontend/lernapp/src/Components/ChatList.js
@@ -1,3 +1,30 @@
-/**
- * alle zugehörigen ChatListEntry des aktiven Users in einem div einfügen
- */
+import React, {Component} from 'react';
+import ChatListEntry from './ChatListEntry';
+import {BankAPI} from '../api';
+
+class ChatList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            Chats: []
+        }
+    }
+
+    get_Chats = () => {
+        BankAPI.getAPI().get_chatsForUser(this.props.profile.getID()).then(ChatroomBOs =>
+            this.setState({
+                Chats: ChatroomBOs
+            })).catch(e =>
+                this.setState({
+                    Chats: []
+                }))
+    }
+
+    componentDidMount(){
+        //get
+    }
+
+    addChatroom = () => {
+        
+    }
+}
