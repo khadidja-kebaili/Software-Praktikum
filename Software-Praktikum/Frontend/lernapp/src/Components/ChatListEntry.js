ec5faba7aa6b687904fdb724cbf0e32d61cd308b
HamiDuong <hd028@hdm-stuttgart.de>
2021-05-13 17:54:35 +0200

    Component ChatListEntry erstellt

--- Software-Praktikum/Frontend/lernapp/src/Components/ChatListEntry.js
+++ Software-Praktikum/Frontend/lernapp/src/Components/ChatListEntry.js
@@ -1,4 +1,37 @@
-/**
- * einzelne Chaträume in einer Liste darstellen
- * wenn members = 2 -> Chatname = Name der Person mit der man schreibt
- * mehr als 2 member -> Gruppename, hier handelt es sich um einen Gruppenchat*/
+import React, {Component} from 'react';
+
+class ChatlistEntry extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: ""
+        }
+    }
+
+    componentDidMount(){
+        //get
+    }
+
+    componentDidUpdate(prevProps){
+        if((this.props.show)&&(this.props.show !== prevProps.show)){
+            //get
+        }
+    }
+
+    //get Methoden
+
+
+    /**
+     * Rendern der Komponente
+     */
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+//Stylesheet 
+const styles;
+
+export default withStyles(styles)(ChatlistEntry);
