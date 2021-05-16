802b5934f2838dac64bf0978f6aa3fcc673a46d3
Lena <lt035@hdm-stuttgart.de>
2021-05-16 11:28:29 +0200

    kleine Änderungen vorgenommen

--- Software-Praktikum/Frontend/lernapp/src/Components/Dialog/GroupDelete.js
+++ Software-Praktikum/Frontend/lernapp/src/Components/Dialog/GroupDelete.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
-import { ElectivAPI } from '../../api';
-import Snackbar from '@material-ui/core/Snackbar';
-import Alert from '@material-ui/lab/Alert';
+import Button from '@material-ui/core/Button';
+import LernappAPI from "../../API/LernappAPI";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
 class GroupDelete extends Component {
@@ -11,67 +13,43 @@ class GroupDelete extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+    };
+  }
 
+  deleteGroup = () => {
+    let data = 6;
+    LernappAPI.getAPI().deleteGroup(data).then(this.props.onClose(null))
+    };
 
-  //wird aufgerufen, wenn das Dialog geschlossen wird
   handleClose = () => {
+    // console.log(event);
     this.props.onClose(null);
   }
 
-// API Anbindung um die Gruppe über das Backend aus der Datenbank zu löschen
-deleteGroup = () => {
-    ElectivAPI.getAPI().deleteGroup(this.state.Group.id)
-      .then(() => {
-        this.props.getGroup();
-        this.props.onClose(null);
-      }).catch(e => {
-        this.setState({
-          showSnackbar: true
-        })
-      })
-  }
-  // Die Komponente wird gerendert 
-  render() 
-    const { show } = this.props;
-    const { Group } = this.state;
-    return (
-      <div>
-        <Dialog
-          open={show}
-          onClose={this.handleClose}
-          maxWidth='xs'
-        >
-          <DialogTitle>{"Sind Sie sich sicher?"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Wenn Sie diese Gruppe <b>"{Group.name}"</b> löschen möchten, klicken Sie auf "JA".
-                  </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Abbrechen
-                  </Button>
-            <Button onClick={this.deleteGroup} color="primary" autoFocus>
-              Ja
-                  </Button>
-          </DialogActions>
-        </Dialog>
-        <Snackbar open={showSnackbar} autoHideDuration={6000} onClose={this.closeSnackbar}>
-          <Alert onClose={this.closeSnackbar} severity="error">
-            Diese Gruppe kann nicht gelöscht werden
-                </Alert>
-        </Snackbar>
-      </div>
+  /** Renders the component */
+  render() {
+
+        return (
+          <div>
+            <Dialog open={this.props.show}>
+              <DialogTitle id="alert-dialog-title">{"Willst du wirklich diese Gruppe löschen?"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Wenn du auf Löschen klickst wird diese Gruppe endgültig gelöscht. 
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button color="primary" onClick={this.props.onClose} >
+                 Abbrechen
+                </Button>
+                <Button color="primary" onClick={this.deleteGroup} autoFocus>
+                  Löschen
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
     );
   }
-
 }
-// PropTypes 
-GroupDelete.propTypes = {
-  
-  classes: PropTypes.object.isRequired,
-  show: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
-}
-
-export default withStyles(styles)(GroupDelete);
+export default GroupDelete;
