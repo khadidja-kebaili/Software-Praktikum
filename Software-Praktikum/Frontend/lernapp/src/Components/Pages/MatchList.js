02f8d7651ed6581d06311ad2252cce840b84b434
EsraCopuro <ec019@hdm-stuttgart.de>
2021-05-21 13:52:12 +0200

    Header, App.js, MatchList, MatchListEntry funktioniert

--- Software-Praktikum/Frontend/lernapp/src/Components/Pages/MatchList.js
+++ Software-Praktikum/Frontend/lernapp/src/Components/Pages/MatchList.js
@@ -3,112 +3,59 @@ import PropTypes from 'prop-types';
 import { withStyles, Button, TextField, InputAdornment, IconButton, Grid, Typography } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import LernappAPI from '../../API/LernappAPI';
-import Error_Message from '../Dialog/Error_Message';
-import LoadingProcess from '../Dialog/LoadingProcess';
 import MatchListEntry from './MatchListEntry';
-import BusinessObject from '../../API/BusinessObject';
-import ProfileBOs from '../../API/ProfileBO';
 
-//Kommentar
+
+
 class MatchList extends Component {
     constructor(props){
         super(props);
         
-        //Eine leere INIT setzten
+        //Eine leere INIT setzten für matches
         this.state={
-            profile: [],
-            error: null,
-            // LoadingProcess: false,
+            matches: [],
             
         };
     }
-    getMatchmaking = () => {
-         let data= 3
-        LernappAPI.getAPI().getMatchmaking(data)
-        .then(ProfileBOs =>
-            this.setState({
-                profile: ProfileBOs,
-                LoadingProcess: false,
-                error: null
-            })).catch(e =>
-                this.setState({
-                    profile: [],
-                    LoadingProcess: false,
-                    error: e
-                })
-                );
-                //setzt loading auf true
-            this.setState({
-                LoadingProcess: true,
-                error: null
-            });
-        }
         //Lifecycle Methode wird aufgerufen, wenn die Komponente in den DOM Browser eingefügt wird
         componentDidMount(){
             this.getMatchmaking();
         }
-        //behandelt onExpandedStateChange Ereignisse aus der MatchListEntry Komponente. Toggelt den Zustand von MatchListEntry von dem angegebenen 
-        //ProfilBO (=Eigentlich ist es voraussichtloch so gedacht.).
-        /** 
-        * @param {profile} ProfilBO of the CustomerListEntry to be toggeled
-        */
-        // onExpandedStateChange = profile =>{
-        //   //Kundeneintrag wird dann auf null gesetzt
-        //   let newID = null;
-          
-        //   //Wenn auf profileeintrag geklickt wird, soll es diese erweitern oder reduzieren
-        //   if (profile.getID() !== this.state.expandMatchID){
-        //       //Erweitert den Matcheintrag mit MatchID
-        //       newID = profile.getID();
-        //   }
-        //   this.setState({
-        //       expandedMatchID: newID,
-        //   });
-        // }
-        //Behandelt den onclose Event von MatchForm
-        // matchFormClosed = profile => {
-        //     //profile sind nicht null und wird daher erstellt
-        //     if (profile){
-        //         const newMatchList = [this.state.profile, profile];
-        //         this.setState({
-        //             profile: newMatchList,
-        //             showMatchForm: false
-        //         });
-        //     }else{
-        //         this.setState({
-        //             showMatchForm: false
-        //         });
-        //     }
-        // }
-        //Die Komponente die gerendert werden
-        render(){
-           const {classes,profile}= this.props
-           const{LoadingProcess,error,
-            // showMatchForm
-        }=this.state;
+   
+    
+    // Die Funktion getMatchmaking() soll die matches anzeigen
+    getMatchmaking =() => {
         
-            return(
-                <div className={classes.root}>
-                    
-                        
-            {/* Zeigt die profileListEntry Komponente */}
-                       
-                    {/* <MatchListEntry key={profile.getID()} profile={profile}
-                    // onExpandedStateChange={this.onExpandedStateChange}
-            />    */}
+        LernappAPI.getAPI().getMatchmaking().then(profileBOs =>
+            this.setState({
+                matches:  profileBOs,
+        
+            }))}
 
-            {/* <MatchListEntry>
-                    
-                     
-                    <LoadingProcess show={LoadingProcess}/> */}
-                    <MatchListEntry>Hallo Kaddi</MatchListEntry>
-                    <Error_Message error={error} contextErrorMsg={'Die Liste konnte leider nicht geladen werden.'} onReload={this.getMatchmaking}/>
-                   
+    
+
+        
+    //Die Komponente die gerendert werden
+    render(){
+        const{matches}=this.state
+            return(
+                <div id='head'>
+                    <Grid item>
+                        <Typography id='title'>
+                            Hier sind deine Matches:
+                        </Typography>
+                    </Grid>
+                    {
+                        matches.map(profiles =>
+                            <MatchListEntry key={profiles.getID()} profiles={profiles}/>)
+                    }
                 </div>
-            
-            );     
-        } 
+            )
+    }
+        
 }
+    
+
 
 // Komponenten-Style wird jetzt definiert
 //Das kann natürlich noch geändert werdeb
