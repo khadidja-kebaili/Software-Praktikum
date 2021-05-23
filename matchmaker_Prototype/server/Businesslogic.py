3f62826e119b49030884f0e0987329082f54e7dd
Khadidja <kk154@hdm-stuttgart.de>
2021-05-23 15:08:47 +0200

    Alle bisherigen Funktionen bzgl. des Matchmakers sind nun auskommentiert.

--- matchmaker_Prototype/server/Businesslogic.py
+++ matchmaker_Prototype/server/Businesslogic.py
@@ -81,6 +81,27 @@ class Businesslogic (object):
         with GroupMapper() as mapper:
             mapper.delete(group)
 
+    '''Hier beginnt das eigentliche Matchmaking'''
+    '''Zunächst einmal wird der Score, also der Integer-Wert wie gut 2 Profile zusammenpassen berechnet
+    Dazu gibt es die Methoden get_learning_habits und set_score'''
+
+    '''Zuerst werden die für den Profilvergleich relevanten Attribute der Profile als sogenannte learning-habits 
+    zusammengefasst. Dazu zählen Semester (1-7), Lernzeitraum (studytime), Lernort (studyplace), Lerntyp (learnstyle), 
+     Studiengang (major) und Lernfrequenz (studyfrequenz)
+     Diese Attribute werden als Liste realisiert, um den späteren verlgeichen so effizient und performant wie möglich
+     zu gestalten.'''
+    def get_learning_habbits(self, id):
+        profile = self.get_profile_by_id(id)
+        learning_habbits = [profile.get_studytime(), profile.get_semester(), profile.get_studyplace(),
+                            profile.get_learnstyle(), profile.get_hobbys(), profile.get_major(),
+                            profile.get_studyfrequence()]
+        return learning_habbits
+
+    '''Als nächstes werden die Lerngewohnheiten von einem Profil mit einem anderen verglichen. Dazu gibt es einen
+    match_score. Jedesmal wenn ein Objekt in der Liste der Lerngewohnheiten des 1. Studentens auch in der Liste 
+    der Lerngewohnheiten des 2. Studentens ist, so steigt der match_score um eins.
+    D.h. wenn die beiden Profile 3 gleiche Werte haben (beide sind bspw. im gleichen Semester, gleicher Studiengang 
+    und wollen gleich oft in der Woche lernen), so ist der Score = 3.'''
     def set_score(self, profile1, profile2):
         match_score = 0
         no_match_score = 0
@@ -93,13 +114,11 @@ class Businesslogic (object):
                     no_match_score += 1
         return match_score
 
-    def get_learning_habbits(self, id):
-        profile = self.get_profile_by_id(id)
-        learning_habbits = [profile.get_studytime(), profile.get_semester(), profile.get_studyplace(), profile.get_learnstyle(),
-                profile.get_hobbys(), profile.get_major(), profile.get_studyfrequence()]
-        return learning_habbits
-
-
+    '''Nun möchte man aber nicht nur 2 Profile miteinander vergleichen, sondern man will ein Profil mit allen anderen
+    in der Datenbank verfügbaren Profilen vergleichen. Daher wird hier eine ID gefordert (hier wird später die ID des
+    current-Users übergeben). Das Profil dieser ID wird dann mit allen anderen Profilen verglichen. Damit der Score und
+    die Profile miteinander verknüpft werden können werden diese in einem dict gespeichert. Das dict wird dann sortiert und
+    daraus entsteht eine Liste, die als Werte nur die Profile besitzt, jedoch sind diese geordnet nach dem Score sortiert. '''
     def matching_list(self, id):
         scores = []
         profiles = []
